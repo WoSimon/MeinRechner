49301abdf9ebb99610b700cdb88d267fc10b280d
Simon Wosnitza <95503436+WoSimon@users.noreply.github.com>
2022-05-14 20:16:56 +0200

    Änderung an der sortierung der Items aus der DB

--- app/src/main/java/com/example/meinrechner/MyDatabaseHelper.java
+++ app/src/main/java/com/example/meinrechner/MyDatabaseHelper.java
@@ -59,7 +59,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     Cursor readAlldata(){
-        String query = "SELECT * FROM " + TABLE_NAME;
+        String query = "SELECT * FROM " + TABLE_NAME + " ORDER BY "  + COLUMN_ID + " DESC";
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = null;
